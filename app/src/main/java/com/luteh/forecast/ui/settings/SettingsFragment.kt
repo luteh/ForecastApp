15aa39dfba08dea98016fd10d0b72f11a445d0a1
luteh <luthfanmaftuh@gmail.com>
2019-04-21 16:27:44 +0700

    Part 1 - Add Navigation & App Foundation

---
+++ app/src/main/java/com/luteh/forecast/ui/settings/SettingsFragment.kt
@@ -0,0 +1,10 @@
+package com.luteh.forecast.ui.settings
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by Luthfan Maftuh on 21/04/2019.
+ * Email luthfanmaftuh@gmail.com
+ */
+class SettingsFragment : Fragment() {
+}
